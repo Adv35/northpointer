9681605128f9f231fcb9f071f7a4271f547c2f04
Adv35 <78689485+Adv35@users.noreply.github.com>
2021-02-07 14:54:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+let north = 0
+basic.forever(function () {
+    north = input.compassHeading()
+    if (north > 315 || north <= 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (north > 45 && north <= 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (north > 135 && north <= 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (north > 225 && north <= 315) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
